541eec154580f362c550117b9a7d1e057ad19e1c
viglino <jean-marc.viglino@ign.fr>
2024-01-11 13:28:17 +0100

    [UPD]

--- examples/cog/index.js
+++ examples/cog/index.js
@@ -9,6 +9,7 @@ const source = new GeoTIFF({
   sources: [
     {
       url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
+      // url: 'https://gpf-download-qua.priv.geopf.fr/download/pva/C2034-0041/F1934-2034_0209.tif',
     },
   ],
 });
