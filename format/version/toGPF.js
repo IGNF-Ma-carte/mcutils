f7fae890007b8d31cdd22b881f2fa2df5d32a0e3
viglino <jean-marc.viglino@ign.fr>
2024-02-27 17:10:31 +0100

    [UPD] toGPF

--- format/version/toGPF.js
+++ format/version/toGPF.js
@@ -12,14 +12,14 @@ function carteGPF(options) {
       case 'WMTS': {
         l.wmtsparam.source.url = l.wmtsparam.source.url.replace(/^https:\/\/wxs.ign.fr\/([^\/]*)\/geoportail\/wmts/, 'https://data.geopf.fr/wmts')
         // Legend
-        if (l.wmtsparam.data.legend) {
+        if (l.wmtsparam.data.legend && l.wmtsparam.data.legend.length) {
           l.wmtsparam.data.legend[0] = l.wmtsparam.data.legend[0].replace(/^https:\/\/wxs.ign.fr\/static\/legends/,'https://data.geopf.fr/annexes/ressources/legendes')
         }
         break;
       }
       case 'WMS': {
         l.wmsparam.source.url = l.wmsparam.source.url.replace(/^https:\/\/wxs.ign.fr\/[^\/]*\/geoportail\/([r|v])\/wms/, 'https://data.geopf.fr/wms-$1/wms')
-        if (l.wmsparam.legend) {
+        if (l.wmsparam.legend && l.wmsparam.legend.length) {
           l.wmsparam.legend[0] = l.wmsparam.legend[0].replace(/^https:\/\/wxs.ign.fr\/static\/legends/,'https://data.geopf.fr/annexes/ressources/legendes')
         }
         break;
