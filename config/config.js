7feaa0c3dfad92128e3ef1a761276097ed4efa1c
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2024-05-14 16:19:44 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,13 +3,15 @@ import importConfig from '../config/import'
 // Default options
 let options = {
   server: 'https://server.ign.fr/',
-  viewer: 'https://server/$TYPE/$ID/$TITLE',
-  userProfile: 'https://server/user/$NAME',
-  editor: 'https://server/edition/$TYPE/$ID',
-  faq: 'https://server/aide/faq/$ARTICLE',
-  tuto: 'https://server/aide/tuto/$ARTICLE',
-  version: 'https://server/aide/notes-de-version/$ARTICLE',
-  sitePiwik: 39,
+  viewer: 'SERVER/carte/$ID/$TITLE?$NOTITLE',
+  userProfile: 'SERVER/utilisateur/$NAME',
+  teamProfile: 'SERVER/equipe/$NAME',
+  editor: 'SERVER/edition/$TYPE/$ID',
+  faq: 'SERVER/aide/faq/$ARTICLE',
+  tuto: 'SERVER/aide/tuto/$ARTICLE',
+  version: 'SERVER/aide/notes-de-version/$ARTICLE',
+  guichetURL: 'https://qlf-collaboratif.cegedim-hds.fr/collaboratif-4.0/',
+  // sitePiwik: 39,
   gppKey: 'k1RSRVIYRxteMEcPK9A5c7g0C6KRw4KX',
 };
 
@@ -22,6 +24,11 @@ if (window.maCarteOptions) {
     options[i] = window.maCarteOptions[i];
   }
   delete window.maCarteOptions;
+  // Update server url
+  const server = options.server.replace(/\/$/,'');
+  for (let i in options) {
+    if (options[i].replace) options[i] = options[i].replace(/^SERVER/, server)
+  };
 } else {
   console.error('NO CONFIG FILE!\n Ajouter un fichier confg.json dans le répertoire des assets...');
 }
