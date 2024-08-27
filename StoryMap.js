2b5f9d9906878e33bd2fac261417a448b47ddd8f
viglino <jean-marc.viglino@ign.fr>
2024-08-27 09:48:04 +0200

    [UPD] animation step

--- StoryMap.js
+++ StoryMap.js
@@ -713,7 +713,7 @@ StoryMap.prototype.setStep = function(n, anim) {
     // Delete previous animation
     setTimeout(() => this.cartes[0].map.getView().cancelAnimations());
     // Map position
-    if (anim===false) {
+    if (anim===false || s.animation === 'none') {
       this.cartes[0].map.getView().setCenter(s.center);
       this.cartes[0].map.getView().setZoom(s.zoom);
     } else {
