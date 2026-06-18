38642c76da04c11635f59666bba4dd4e5619bc55
viglino <jean-marc.viglino@ign.fr>
2026-06-18 10:40:04 +0200

    [UPD] Matomo

--- charte/setMatomo.js
+++ charte/setMatomo.js
@@ -13,7 +13,35 @@ export default function(siteId) {
     var u="https://matomo.ign.fr/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', siteId]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    var d=document, 
+      g=d.createElement('script'), 
+      s=d.getElementsByTagName('script')[0];
+    g.async=true; 
+    g.src=u+'matomo.js'; 
+    s.parentNode.insertBefore(g,s);
   })();
+
+  var _mtm = window._mtm = window._mtm || [];
+  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+  /** /
+  (function() {
+    var d=document, 
+      g=d.createElement('script'), 
+      s=d.getElementsByTagName('script')[0];
+    g.async=true; 
+    g.src='https://matomo.ign.fr/js/container_FImKsPkT.js'; 
+    s.parentNode.insertBefore(g,s);
+  })();
+  /*/
+  (function() {
+    var d = document
+      , g = d.createElement('script')
+      , s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript';
+    g.async = false;
+    g.defer = false;
+    g.src = 'https://matomo.ign.fr/js/container_FImKsPkT.js';
+    s.parentNode.insertBefore(g, s);
+  })();
+  /**/
 }
